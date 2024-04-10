2e2887f87d5b30aa9a8e74ecadab99a7ae0c8a85
hsuehyt <hsueh.yuting@gmail.com>
2024-04-11 00:39:38 +0800

    explanatory notes added; botton to button

--- MyOperators.cs
+++ MyOperators.cs
@@ -5,20 +5,24 @@ using TMPro;
 
 public class MyOperators : MonoBehaviour
 {
-    private TextMeshProUGUI TextMeshPro;
+    private TextMeshProUGUI TextMeshPro; // Declare a private field to hold a TextMeshProUGUI component
+
     // Start is called before the first frame update
     void Start()
     {
-        TextMeshPro = GetComponent<TextMeshProUGUI>(); 
+        TextMeshPro = GetComponent<TextMeshProUGUI>(); // Assign the TextMeshProUGUI component attached to this game object
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Currently, the Update method is empty (no code inside)
+        // You can add logic here if needed for frame-by-frame updates
     }
+
+    // Custom method to display a message
     public void DisplayMessage()
     {
-        TextMeshPro.text = "Botton Clicked!";
+        TextMeshPro.text = "Button Clicked!"; // Set the text of the TextMeshProUGUI component to "Button Clicked!"
     }
 }
